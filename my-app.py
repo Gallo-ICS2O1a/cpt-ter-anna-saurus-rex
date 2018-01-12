e09a02e355a763789117812a0aaf8bee18985b6e
teresa_vu <33972090+tersavu@users.noreply.github.com>
2018-01-12 01:03:04 -0500

    Update my-app.py

--- my-app.py
+++ my-app.py
@@ -1,14 +1,14 @@
 # Template for Processing sketches.
 scene = 1
 guess = 0
-box4_loc = PVector(462, 305)
-box4_size = PVector(60, 80)
-box2_loc = PVector(193, 305)
-box2_size = PVector(60, 80)
 box1_loc = PVector(66, 305)
 box1_size = PVector(60, 80)
+box2_loc = PVector(193, 305)
+box2_size = PVector(60, 80)
 box3_loc = PVector(330, 305)
 box3_size = PVector(60, 80)
+box4_loc = PVector(462, 305)
+box4_size = PVector(60, 80)
 def setup():
     size(600, 600)
     
@@ -70,7 +70,7 @@ def mousePressed():
         background(255)
         textSize(30)
         fill(172, 83, 83)
-        text("How many Grammys did Kanye earn?", 50, 200)
+        text("How many Grammys did Kanye earn?", 45, 200)
         fill(0, 0, 255)
         textSize(50)
         text("24", 68, 300)
@@ -123,8 +123,8 @@ def mousePressed():
                 
     if scene == 5:
             fill(255, 77, 77)
-            textSize(16)
-            text("The correct answer is.... 21!", 50, 500)
+            textSize(20)
+            text("The correct answer is.... 21!", 150, 500)
     
     if scene == 6:
         background(255)
@@ -184,8 +184,8 @@ def mousePressed():
     
     if scene == 8:
         fill(255, 77, 77)
-        textSize(16)
-        text("The correct answer is...Tesfaye!", 190, 500)
+        textSize(20)
+        text("The correct answer is...Tesfaye!", 150, 500)
         
     if scene == 9:
         background(255)
@@ -194,18 +194,61 @@ def mousePressed():
         text("How many McDonald's are there in Canada as of 2014?", 50, 200)
         fill(0, 0, 255)
         textSize(30)
-        text("5000      1400       2400       2500", 80, 300)
-        rect(352, 305, 60, 80)
-        rect(217, 305, 60, 80)
-        rect(83, 305, 60, 80)
-        rect(500, 305, 60, 80)
+        text("5000", 63, 300)
+        fill(0, 0, 255)
+        textSize(30)
+        text("1400", 190, 300)
+        fill(0, 0, 255)
+        textSize(30)
+        text("2400", 327, 300)
+        fill(0, 0, 255)
+        textSize(30)
+        text("2500", 459, 300)
+        rect(330, 305, 60, 80)
+        rect(193, 305, 60, 80)
+        rect(66, 305, 60, 80)
+        rect(462, 305, 60, 80)
         
     if scene == 10:
-        fill(255, 77, 77)
-        textSize(16)
-        text("The correct answer is...1400!", 211, 500)
+        if mouseX >= box2_loc.x and mouseX <= box2_loc.x + box2_size.x:
+            if mouseY >= box2_loc.y and mouseY <= box2_loc.y + box2_size.y:
+                fill(50, 205, 50)
+                rect(193, 305, 60, 80)
+                fill(50, 205, 50)
+                textSize(30)
+                text("1400", 190, 300)
+                guess += 1
+        elif mouseX >= box1_loc.x and mouseX <= box1_loc.x + box1_size.x:
+            if mouseY >= box1_loc.y and mouseY <= box1_loc.y + box1_size.y:
+                fill(255, 0, 0)
+                rect(66, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("5000", 63, 300)
+                guess -= 1 
+        elif mouseX >= box4_loc.x and mouseX <= box4_loc.x + box4_size.x:
+            if mouseY >= box4_loc.y and mouseY <= box4_loc.y + box4_size.y:
+                fill(255, 0, 0)
+                rect(462, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("2500", 459, 300)
+                guess -= 1 
+        elif mouseX >= box3_loc.x and mouseX <= box3_loc.x + box3_size.x:
+            if mouseY >= box3_loc.y and mouseY <= box3_loc.y + box3_size.y:
+                fill(255, 0, 0)
+                rect(330, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("2400", 327, 300)
+                guess -= 1 
         
     if scene == 11:
+        fill(255, 77, 77)
+        textSize(20)
+        text("The correct answer is...1400!", 150, 500)
+        
+    if scene == 12:
         background(255)
         textSize(30)
         fill(172, 83, 83)
@@ -218,10 +261,7 @@ def mousePressed():
         text("BCE", 70, 300)
         fill(0, 0, 255)
         textSize(30)
-        text("8500", 187, 275)
-        fill(0, 0, 255)
-        textSize(30)
-        text("BCE", 197, 300)
+        text("1375", 187, 300)
         fill(0, 0, 255)
         textSize(30)
         text("7000", 324, 275)
@@ -235,30 +275,68 @@ def mousePressed():
         rect(193, 305, 60, 80)
         rect(66, 305, 60, 80)
         rect(462, 305, 60, 80)
-    
-    if scene == 12:
-        fill(255, 77, 77)
-        textSize(16)
-        text("The correct answer is...8000 BCE!", 175, 500)
-       
     if scene == 13:
+        if mouseX >= box1_loc.x and mouseX <= box1_loc.x + box1_size.x:
+            if mouseY >= box1_loc.y and mouseY <= box1_loc.y + box1_size.y:
+                fill(50, 205, 50)
+                rect(66, 305, 60, 80)
+                fill(50, 205, 50)
+                textSize(30)
+                text("8000", 60, 275)
+                fill(50, 205, 50)
+                textSize(30)
+                text("BCE", 70, 300)
+                guess += 1
+        elif mouseX >= box2_loc.x and mouseX <= box2_loc.x + box2_size.x:
+            if mouseY >= box2_loc.y and mouseY <= box2_loc.y + box2_size.y:
+                fill(255, 0, 0)
+                rect(193, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("1375", 187, 300)
+                guess -= 1 
+        elif mouseX >= box4_loc.x and mouseX <= box4_loc.x + box4_size.x:
+            if mouseY >= box4_loc.y and mouseY <= box4_loc.y + box4_size.y:
+                fill(255, 0, 0)
+                rect(462, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("1928", 455, 300)
+                guess -= 1 
+        elif mouseX >= box3_loc.x and mouseX <= box3_loc.x + box3_size.x:
+            if mouseY >= box3_loc.y and mouseY <= box3_loc.y + box3_size.y:
+                fill(255, 0, 0)
+                rect(330, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("7000", 324, 275)
+                fill(255, 0, 0)
+                textSize(30)
+                text("BCE", 334, 300)
+                guess -= 1 
+    if scene == 14:
+        fill(255, 77, 77)
+        textSize(20)
+        text("The correct answer is...8000 BCE!", 150, 500)
+       
+    if scene == 15:
         background(255)
         textSize(24)
         fill(172, 83, 83)
         text("What does durry mean in Australian slang?", 50, 200)
         fill(0, 0, 255)
         textSize(30)
-        text("Toilet", 60, 275)
+        text("Toilet", 55, 300)
         fill(0, 0, 255)
         textSize(30)
         fill(0, 0, 255)
-        textSize(25)
-        text("Cigerette", 187, 275)
+        textSize(30)
+        text("Friend", 180, 300)
         fill(0, 0, 255)
         textSize(25)
         fill(0, 0, 255)
-        textSize(25)
-        text("Friend", 324, 285)
+        textSize(26)
+        text("Cigarette", 310, 300)
         fill(0, 0, 255)
         textSize(30)
         fill(0, 0, 255)
@@ -268,3 +346,42 @@ def mousePressed():
         rect(193, 305, 60, 80)
         rect(66, 305, 60, 80)
         rect(462, 305, 60, 80)
+        
+    if scene == 16:
+        if mouseX >= box3_loc.x and mouseX <= box3_loc.x + box3_size.x:
+            if mouseY >= box3_loc.y and mouseY <= box3_loc.y + box3_size.y:
+                fill(50, 205, 50)
+                rect(330, 305, 60, 80)
+                fill(50, 205, 50)
+                textSize(26)
+                text("Cigarette", 310, 300)
+                guess += 1
+        elif mouseX >= box2_loc.x and mouseX <= box2_loc.x + box2_size.x:
+            if mouseY >= box2_loc.y and mouseY <= box2_loc.y + box2_size.y:
+                fill(255, 0, 0)
+                rect(193, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("Friend", 180, 300)
+                guess -= 1 
+        elif mouseX >= box4_loc.x and mouseX <= box4_loc.x + box4_size.x:
+            if mouseY >= box4_loc.y and mouseY <= box4_loc.y + box4_size.y:
+                fill(255, 0, 0)
+                rect(462, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("Bomb", 455, 300)
+                guess -= 1 
+        elif mouseX >= box1_loc.x and mouseX <= box1_loc.x + box1_size.x:
+            if mouseY >= box1_loc.y and mouseY <= box1_loc.y + box1_size.y:
+                fill(255, 0, 0)
+                rect(66, 305, 60, 80)
+                fill(255, 0, 0)
+                textSize(30)
+                text("Toilet", 55, 300)
+                guess -= 1 
+                
+    if scene == 17:
+        fill(255, 77, 77)
+        textSize(20)
+        text("The correct answer is...Cigarette!", 150, 500)
